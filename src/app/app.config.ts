4dc456cf9d4b688806b925cfe4b4e11227dc9d6d
Jordan-182 <Jordan.P62@outlook.fr>
2025-09-20 16:54:27 +0200

    ajout du formulaire de modification d'un pokemon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,16 @@ import {
 } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found';
+import { PokemonEdit } from './pokemon/pokemon-edit/pokemon-edit';
 import { PokemonList } from './pokemon/pokemon-list/pokemon-list';
 import { PokemonProfile } from './pokemon/pokemon-profile/pokemon-profile';
 
 const routes: Routes = [
+  {
+    path: 'pokemons/edit/:id',
+    component: PokemonEdit,
+    title: 'Pokémon',
+  },
   { path: 'pokemons/:id', component: PokemonProfile, title: 'Pokémon' },
   { path: 'pokemons', component: PokemonList, title: 'Pokédex' },
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
